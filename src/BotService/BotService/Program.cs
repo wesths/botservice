b7f43c3b1b24afe56b37d3a1cba14035e7410945
Shane van der Westhuizen <svdwesthuizen@homechoice.co.za>
2018-02-24 17:54:17 +0200

    added usekestrel

--- src/BotService/BotService/Program.cs
+++ src/BotService/BotService/Program.cs
@@ -20,6 +20,7 @@ namespace BotService
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+            .UseKestrel()
                 .Build();
     }
 }
